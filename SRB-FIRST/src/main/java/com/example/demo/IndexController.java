07b2c42cb2cc3ccdcb6bbc7a39731280d42775d9
srv <srv@DESKTOP-HTURIMR>
2019-04-17 13:53:36 +0900

    UTF-8 변경후 ..

--- SRB-FIRST/src/main/java/com/example/demo/IndexController.java
+++ SRB-FIRST/src/main/java/com/example/demo/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	@RequestMapping(value = "/index" , method=RequestMethod.GET) 
 	public String getIndex() { 
 		
-		//�׽�Ʈ �Դϴ�.
+		//테스트 파일입니다.
 		
 		return "index";
 	}
